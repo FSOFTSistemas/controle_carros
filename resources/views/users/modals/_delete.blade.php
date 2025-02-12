2b65da7a10e6856fc0572d868fedc511aa87b7a4
PauloHDuque <fsoftsistemas@gmail.com>
2025-02-12 10:42:33 -0300

    fix: botão fechar modal

--- resources/views/users/modals/_delete.blade.php
+++ resources/views/users/modals/_delete.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="deleteUserModalLabel">Excluir Usuário</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <p>Tem certeza que deseja excluir este usuário?</p>
